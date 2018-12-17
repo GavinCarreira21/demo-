3fca811b40e73c90c6ef65fe59d4be1093e62da0
GavinCarreira21 <44782513+GavinCarreira21@users.noreply.github.com>
2018-12-16 20:26:49 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 # this file was created by Gavin Carreira
-# Sources: goo.gl/2KMivS 
-# now available in github
+# Sources: goo.gl/2KMivS - Chris Bradfield your the man!
+# availble in github
+#https://www.youtube.com/channel/UCNaPQ5uLX5iIEHUCLmfAgKg
+#https://www.zapsplat.com/?s=jump
+#https://www.youtube.com/watch?v=qnUVjACD3WM
 
+'''
+**********Gameplay ideas:
+have bullets shoot out of the enemy to destroy incoming mobs 
+
+*****failed ideas 
+My main goal in creating this project was to have the chracter fire bullets at the 
+incoming eneimies, much like a concept from the orginal doodle jump. I started out watching Kids can code 
+videos and coding along, but later realized I was way over my head and some of the code I was beginning 
+to put was stuff I had never seen before, this ultimatley caused me to start over
+
+**********Cosmetics
+change clouds, enemies, sound, jump, explorer
+
+**********Bugs
+will not read spritesheet... "name 'Spritesheet' is not defined"
+
+**********Gameplay fixes
+increased boost and jump to decrese impossible jump zones 
+
+**********Features
+increased mobs, with added boosts, and higher powerups and jumps 
+'''
 import pygame as pg
 import random
 from settings import *
@@ -9,6 +34,7 @@ from sprites import *
 from os import path
 
 class Game:
+    ##### INIT METHODF
     def __init__(self):
         #init game window
         # init pygame and create window
@@ -16,14 +42,15 @@ class Game:
         # init sound mixer
         pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption("jumping goov")
+        pg.display.set_caption("Space explorer")
         self.clock = pg.time.Clock()
         self.running = True
         self.font_name = pg.font.match_font(FONT_NAME)
         self.load_data()
+    ##### LOAD DATA METHOD
     def load_data(self):
         print("load data is called...")
-        # sets up directory name
+        # sets up directory name for images
         self.dir = path.dirname(__file__)
         img_dir = path.join(self.dir, 'img')
         # opens file with write options
@@ -39,26 +66,35 @@ class Game:
             with open(path.join(self.dir, HS_FILE), 'w') as f:
                 self.highscore = 0
                 print("exception")
-        # load spritesheet image
-        self.spritesheet = Spritesheet(path.join(img_dir, SPRITESHEET))       
+        # loads spritesheet image
+        self.spritesheet = Spritesheet(path.join(img_dir, SPRITESHEET)) 
+        #loads space portal images
+        self.cloud_images = []
+        for i in range(1,4):
+            self.cloud_images.append(pg.image.load(path.join(img_dir, 'cloud{}.png'.format(i))).convert())
         # load sounds
-        # great place for creating sounds: https://www.bfxr.net/
+        # awesome place to find sounds - https://www.zapsplat.com/?s=jump 
         self.snd_dir = path.join(self.dir, 'snd')
-        self.jump_sound = [pg.mixer.Sound(path.join(self.snd_dir, 'Jump18.wav')),
-                            pg.mixer.Sound(path.join(self.snd_dir, 'Jump24.wav'))]
-        self.boost_sound = pg.mixer.Sound(path.join(self.snd_dir, 'Jump29.wav'))
-
-                            
+        self.jump_sound = [pg.mixer.Sound(path.join(self.snd_dir, 'boing.mp3')),
+                            pg.mixer.Sound(path.join(self.snd_dir, 'Jump29.wav'))]
+        self.boost_sound = pg.mixer.Sound(path.join(self.snd_dir, 'swoosh.mp3'))
+        self.head_jump_sound = pg.mixer.Sound(path.join(self.snd_dir, 'footjump.mp3'))
+    ##### NEW METHOD
     def new(self):
         self.score = 0
+        self.paused = False
         # add all sprites to the pg group
         # below no longer needed - using LayeredUpdate group
         # self.all_sprites = pg.sprite.Group()
         self.all_sprites = pg.sprite.LayeredUpdates()
         # create platforms group
         self.platforms = pg.sprite.Group()
+        # create clouds group
+        self.clouds = pg.sprite.Group()
         # add powerups
         self.powerups = pg.sprite.Group()
+        # add cacti
+        self.cacti = pg.sprite.Group()
         
         self.mob_timer = 0
         # add a player 1 to the group
@@ -75,13 +111,17 @@ class Game:
             # no longer needed because we pass in Sprite lib file
             # self.all_sprites.add(p)
             # self.platforms.add(p)
-        # load music
-        pg.mixer.music.load(path.join(self.snd_dir, 'happy.ogg'))
+        for i in range(8):
+            c = Cloud(self)
+            c.rect.y += 500
+        # load spaceship/shuttle music 
+        pg.mixer.music.load(path.join(self.snd_dir, 'spaceshuttle.mp3'))
         # call the run method
         self.run()
     def run(self):
         # game loop
         # play music
+        # for spaceshuttle music 
         pg.mixer.music.play(loops=-1)
         # set boolean playing to true
         self.playing = True
@@ -91,48 +131,62 @@ class Game:
             self.update()
             self.draw()
         pg.mixer.music.fadeout(1000)
+        # other things that happen when not playing anymore
+    ##### UPDATE METHOD
     def update(self):
         self.all_sprites.update()
-        
         # shall we spawn a mob?
         now = pg.time.get_ticks()
+        # check for mob collisions
         if now - self.mob_timer > 5000 + random.choice([-1000, -500, 0, 500, 1000]):
             self.mob_timer = now
             Mob(self)
-        # check for mob collisions
-        mob_hits = pg.sprite.spritecollide(self.player, self.mobs, False)
+        # now using collision mask to determine collisions
+        # can use rectangle collisions here first if we encounter performance issues
+        mob_hits = pg.sprite.spritecollide(self.player, self.mobs, False, pg.sprite.collide_mask)
         if mob_hits:
-            if self.player.pos.y - 35 < mob_hits[0].rect_top:
+            # can use mask collide here if mob count gets too high and creates performance issues
+            ''' I created the below code as an added feature:
+                I wanted to create an option to jump on the enemies'
+                heads to prevent getting stuck...'''
+            if self.player.pos.y - 35 < mob_hits[0].rect.top:
                 print("hit top")
                 print("player is " + str(self.player.pos.y))
-                print("mob is " + str(mob_hits[0].rect_top))
+                print("mob is " + str(mob_hits[0].rect.top))
+                self.head_jump_sound.play()
                 self.player.vel.y = -BOOST_POWER
             else:
                 print("player is " + str(self.player.pos.y))
-                print("mob is " + str(mob_hits[0].rect_top))
+                print("mob is " + str(mob_hits[0].rect.top))
                 self.playing = False
-
         # check to see if player can jump - if falling
         if self.player.vel.y > 0:
             hits = pg.sprite.spritecollide(self.player, self.platforms, False)
             if hits:
-                # set var to be current hit in list to find which to 'pop' to when two or more collide with player
+                ''' set var to be current hit in list to find which to 'pop' to 
+                when two or more collide with player'''
                 find_lowest = hits[0]
                 for hit in hits:
                     if hit.rect.bottom > find_lowest.rect.bottom:
                         print("hit rect bottom " + str(hit.rect.bottom))
                         find_lowest = hit
                 # fall if center is off platform
-                if self.player.pos.x < find_lowest.rect.right + 10 and self.player.pos.x > find_lowest.rect.left - 10:
+                if self.player.pos.x < find_lowest.rect.right + 5 and self.player.pos.x > find_lowest.rect.left - 5:
                     if self.player.pos.y < find_lowest.rect.centery:
                         self.player.pos.y = find_lowest.rect.top
                         self.player.vel.y = 0
                         self.player.jumping = False
-                # scroll plats with player
+        # if player reaches top 25th percentile of the screen spawn a portal....
         if self.player.rect.top <= HEIGHT / 4:
-            # creates slight scroll at the top based on player y velocity
+            # spawn a portal 
+            if randrange(100) < 13:
+                Cloud(self)
+            # set player location based on velocity
             self.player.pos.y += max(abs(self.player.vel.y), 2)
-            
+            for cloud in self.clouds:
+                cloud.rect.y += max(abs(self.player.vel.y / randrange(2,10)), 2)
+            # creates slight scroll at the top based on player y velocity
+            # scroll plats and mobs with player
             for mob in self.mobs:
                 # creates slight scroll based on player y velocity
                 mob.rect.y += max(abs(self.player.vel.y), 2)
@@ -149,26 +203,34 @@ class Game:
                 self.boost_sound.play()
                 self.player.vel.y = -BOOST_POWER
                 self.player.jumping = False
-        
-        # Die!
+        cacti_hits = pg.sprite.spritecollide(self.player, self.cacti, False)
+        if cacti_hits:    
+            if self.player.vel.y > 0 and self.player.pos.y > cacti_hits[0].rect.top:
+                    print("falling")
+                    print("player is " + str(self.player.pos.y))
+                    print("mob is " + str(cacti_hits[0].rect.top))
+        # Die space explorer!
         if self.player.rect.bottom > HEIGHT:
+            '''make all sprites fall up when player falls'''
             for sprite in self.all_sprites:
                 sprite.rect.y -= max(self.player.vel.y, 10)
-                if sprite.rect.bottom < 0:
+                '''get rid of sprites as they fall up'''
+                if sprite.rect.bottom < -25:
                     sprite.kill()
         if len(self.platforms) == 0:
             self.playing = False
         # generate new random platforms
         while len(self.platforms) < 6:
             width = random.randrange(50, 100)
-            ''' removed widths and height params to allow for sprites'''
-            # changed due to passing into groups through sprites lib file
+            ''' removed widths and height params to allow for sprites '''
+            """ changed due to passing into groups through sprites lib file """
             # p = Platform(self, random.randrange(0,WIDTH-width), 
             #                 random.randrange(-75, -30))
             Platform(self, random.randrange(0,WIDTH-width), 
                             random.randrange(-75, -30))
             # self.platforms.add(p)
             # self.all_sprites.add(p)
+    ##### EVENTS METHOD
     def events(self):
         for event in pg.event.get():
                 if event.type == pg.QUIT:
@@ -180,16 +242,22 @@ class Game:
                         self.player.jump()
                 if event.type == pg.KEYUP:
                     if event.key == pg.K_SPACE:
-                        # cuts the jump short if the space bar is released
+                        """ # cuts the jump short if the space bar is released """
                         self.player.jump_cut()
+                if event.type == pg.KEYUP:
+                    if event.key == pg.K_p:
+                        """ pause """
+                        self.paused = True
+    ##### DRAW METHOD
     def draw(self):
         self.screen.fill(SKY_BLUE)
         self.all_sprites.draw(self.screen)
-        # not needed now that we're using LayeredUpdates
+        """ # not needed now that we're using LayeredUpdates """
         # self.screen.blit(self.player.image, self.player.rect)
         self.draw_text(str(self.score), 22, WHITE, WIDTH / 2, 15)
         # double buffering - renders a frame "behind" the displayed frame
         pg.display.flip()
+    ##### WAIT FOR KEY METHOD
     def wait_for_key(self): 
         waiting = True
         while waiting:
@@ -200,8 +268,9 @@ class Game:
                     self.running = False
                 if event.type ==pg.KEYUP:
                     waiting = False
+    ##### SHOW START SCREEN METHOD
     def show_start_screen(self):
-        # game splash screen
+        """ # game splash screen """
         self.screen.fill(BLACK)
         self.draw_text(TITLE, 48, WHITE, WIDTH/2, HEIGHT/4)
         self.draw_text("WASD to move, Space to jump", 22, WHITE, WIDTH/2, HEIGHT/2)
@@ -209,8 +278,9 @@ class Game:
         self.draw_text("High score " + str(self.highscore), 22, WHITE, WIDTH / 2, 15)
         pg.display.flip()
         self.wait_for_key()
+    ##### SHOW GO SCREEN METHOD
     def show_go_screen(self):
-        # game splash screen
+        """ # game splash screen """
         if not self.running:
             print("not running...")
             return
@@ -231,6 +301,7 @@ class Game:
 
         pg.display.flip()
         self.wait_for_key()
+    ##### DRAW TEXT METHOD
     def draw_text(self, text, size, color, x, y):
         font = pg.font.Font(self.font_name, size)
         text_surface = font.render(text, True, color)
--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
-TITLE = "jumpy"
-# screen dims
-WIDTH = 480
+TITLE = "Space explorer"
+# screen dims 
+WIDTH = 500
 HEIGHT = 600
-# frames per second
+# fps
 FPS = 60
 # colors
 WHITE = (255, 255, 255)
@@ -11,23 +11,26 @@ REDDISH = (240,55,66)
 SKY_BLUE = (143, 185, 252)
 FONT_NAME = 'arial'
 SPRITESHEET = "spritesheet_jumper.png"
+SPRITESHEET2 = "spritesheet.png"
 # data files
 HS_FILE = "highscore.txt"
 # player settings
-PLAYER_ACC = 0.5
+PLAYER_ACC = 0.75
 PLAYER_FRICTION = -0.12
-PLAYER_GRAV = 0.8
-PLAYER_JUMP = 20
+PLAYER_GRAV = 0.9
+PLAYER_JUMP = 43
 # game settings
-BOOST_POWER = 60
-POW_SPAWN_PCT = 7
-MOB_FREQ = 5000
+BOOST_POWER = 70
+POW_SPAWN_PCT = 10
+MOB_FREQ = 700
+# layers - uses numerical value in layered sprites
 PLAYER_LAYER = 2
 PLATFORM_LAYER = 1
 POW_LAYER = 1
 MOB_LAYER = 2
+CLOUD_LAYER = 0
 
-# platform settings
+# platform settings 
 ''' old platforms from drawing rectangles'''
 '''
 PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
@@ -36,7 +39,10 @@ PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
                  (200, HEIGHT - 150, WIDTH-350, 40),
                  (200, HEIGHT - 450, WIDTH-350, 40)]
 '''
-PLATFORM_LIST = [(0, HEIGHT - 40),
+PLATFORM_LIST = [(0, HEIGHT-40),
+                (50, HEIGHT-40),
+                (100, HEIGHT-40),
+                (150, HEIGHT-40),
                  (65, HEIGHT - 300),
                  (20, HEIGHT - 350),
                  (200, HEIGHT - 150),
